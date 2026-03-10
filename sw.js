5a5b29bf2a3dd3b57b85e3bcea09dff45f73f5cf
Stvfen <534320747@qq.com>
2026-03-10 14:07:32 +0800

    Site updated: 2026-03-10 14:07:32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773122797020";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773122848636";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
